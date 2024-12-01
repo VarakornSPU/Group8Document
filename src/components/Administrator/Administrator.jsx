7a1d75e1946d80a4e048a08a621f38e6ceb2a9a6
VarakornSPU <varakon.mat@spumail.net>
2024-12-01 15:59:22 +0700

    Delete source codes

--- src/components/Administrator/Administrator.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/Administrator/Administrator.jsx
-// eslint-disable-next-line no-unused-vars
-import React from "react";
-
-function Administrator() {
-  return <h1>Administrator Component</h1>;
-}
-
-export default Administrator;
